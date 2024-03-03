e07b39ab6402cf4e9d82d2287878dcb8d8c51991
MSCompSci <62676614+MSCompSci@users.noreply.github.com>
2024-03-02 22:41:24 -0500

    Add menu toggle and styling
    Signed-off-by: MSCompSci <62676614+MSCompSci@users.noreply.github.com>

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
   import "@fortawesome/fontawesome-free/css/all.min.css";
   import DarkMode from "@components/darkMode.svelte";
+  let menuOpen: boolean = false;
+  function toggleMenu() {
+    if (!menuOpen) {
+      document.getElementById("navMenu")?.classList.remove("hidden");
+      menuOpen = true;
+    } else {
+      document.getElementById("navMenu")?.classList.add("hidden");
+      menuOpen = false;
+    }
+  }
 </script>
 
 <header class="fixed z-10 top-4 right-4 left-4 text-white">
-  <div class="bg-slate-800 rounded-[2rem]">
+  <div class="bg-slate-800 bg-opacity-70 rounded-[2rem]">
     <menu
       class="mr-0 ml-auto h-fit bg-slate-300 w-fit dark:bg-slate-900 rounded-full flex justify-end p-2 gap-2"
     >
@@ -12,29 +22,51 @@
         <DarkMode />
       </li>
       <li>
-        <button class="h-11 w-11 rounded-full border bg-black">
+        <button
+          on:click={toggleMenu}
+          aria-controls="navMenu"
+          title="Menu"
+          class="h-11 w-11 rounded-full border bg-black"
+        >
           <i aria-hidden="true" class="fa-solid fa-xl fa-bars"></i>
-          <span class="sr-only">Menu</span>
+          <span class="sr-only">Main menu</span>
         </button>
       </li>
     </menu>
-    <nav class="list-none flex justify-center pb-4 rounded-3xl">
+    <nav
+      id="navMenu"
+      class="hidden list-none flex justify-center pb-4 rounded-3xl"
+    >
       <ol class="flex flex-col gap-4 text-center pt-6 w-[66%]">
-        <li class="w-full border py-2 rounded-full">
-          <a class="flex justify-center" href="/#home">Home</a>
+        <li class="w-full border rounded-full">
+          <a
+            title="Home"
+            class="flex justify-center rounded-full py-3"
+            href="/#home">Home</a
+          >
         </li>
-        <li class="w-full border py-2 rounded-full">
-          <a class="flex justify-center" href="/#about">About</a>
+        <li class="w-full border rounded-full">
+          <a
+            title="About"
+            class="flex justify-center rounded-full py-3"
+            href="/#about">About</a
+          >
         </li>
-        <li class="w-full border py-2 rounded-full">
-          <a class="flex justify-center" href="/#projects">Projects</a>
+        <li class="w-full border rounded-full">
+          <a
+            title="Projects"
+            class="flex justify-center rounded-full py-3"
+            href="/#projects">Projects</a
+          >
         </li>
-        <li class="w-full border py-2 rounded-full">
-          <a class="flex justify-center" href="/#contact">Contact</a>
+        <li class="w-full border rounded-full">
+          <a
+            title="Contact"
+            class="flex justify-center rounded-full py-3"
+            href="/#contact">Contact</a
+          >
         </li>
       </ol>
     </nav>
   </div>
-  <!-- 
-   -->
 </header>
