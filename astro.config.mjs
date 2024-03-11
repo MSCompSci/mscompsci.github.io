e66fbac241dc81d6a87b75c80f511c3dc8291d4c
MSCompSci <62676614+MSCompSci@users.noreply.github.com>
2024-03-11 14:23:25 -0400

    Add config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mscompsci.github.io',
   integrations: [svelte(), tailwind({
     applyBaseStyles: false
   }), mdx()]
